8e20781d42e77d635e18b7ec4509a74f83824e78
vainavibalasubramanian <vainavibalasubramanian@gmail.com>
2025-10-30 22:34:41 +0530

    ui changes

--- src/MainPage/Home.js
+++ src/MainPage/Home.js
@@ -4,8 +4,8 @@ import "./Home.css"; // optional for styling
 function Home() {
   return (
     <div className="home-container">
-      <h1>Welcome to AI Model Dashboard</h1>
-      <p>Select a model to test:</p>
+      <h1 style={{color: "#f0f0f0", marginRight: "600px"}}>Welcome to AI Model Dashboard</h1>
+      <p style={{color: "#f0f0f0", marginRight: "600px"}}>Select a model to test:</p>
 
       <div className="button-container">
         <Link to="/knn" className="nav-button">KNN Classifier</Link>
